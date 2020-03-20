6ca29bf54ae71667f543247c6e0f20b2fb999235
MacRent <macrent@MacRentui-MacBook-Pro-5.local>
2020-03-20 09:42:31 +0900

    각도측정

--- wave.sql
+++ wave.sql
@@ -7,7 +7,8 @@ select count(*) from _land where pub_price is null;
 select 
 (select pub_price from _land l where al.id=l.id), 
 (select array_agg(l2.pub_price) from _land l2 where l2.id in (select unnest(al.array_agg))),
-(select st_azimuth( st_centroid( (select geom from _land l3 where l3.id=al.id) ), st_centroid( (select geom from _land l4 where l4.id in (select unnest(al.array_agg) limit 1) ) ) ))
-from adj_level2 al ;
+(select array_agg(st_azimuth( st_centroid( (select geom from _land l3 where l3.id=al.id) ), st_centroid(geom) )) from _land l4 where l4.id in (select unnest(al.array_agg)) )
+from adj_level2 al ; -- 인접대지들과 대상 대지의 중심점끼리 각도 
+
 
 
